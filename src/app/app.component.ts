b70b69050f34c61a1ac1670a7097d1310dad914e
adrian.leon <adrian.leon@WK-LEOA-2>
2024-06-21 09:03:39 +0200

    Adapt to only angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DxListModule, DxTemplateModule } from 'devextreme-angular';
+import { RouterOutlet, Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import DataSource from 'devextreme/data/data_source';
-import ArrayStore from 'devextreme/data/array_store';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,    
-    DxListModule,
-    DxTemplateModule,
-    RouterModule],
+    RouterModule,
+    CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,21 +17,13 @@ import ArrayStore from 'devextreme/data/array_store';
 export class AppComponent {
   title = 'devextreme-routing';
 
-  secondaryRoute(): DataSource {
-
-    return new DataSource({
-      store: new ArrayStore({
-        data: [{
-          key: 'Group components',
-          items: [{text: 'Go first', path: 'first'}, {text: 'Go second', path: 'second'}],
-        }],
-      }),
-      map: (dataItem) => {
-        return {
-          key: dataItem.key,
-          items: dataItem.items,
-        };
-      },
+  secondaryRoute(): Routes {
+    const a = [{text: 'Go first', path: 'first'}, {text: 'Go second', path: 'second'}];
+    return a.map((route) => {
+      return {
+        title: route.text,
+        path: route.path,
+      };
     });
   }
 }
