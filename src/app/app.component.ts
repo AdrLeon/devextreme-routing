e0964daa0d6f6bb6fc9bbbb8d1b58e3b3f31b384
adrian.leon <adrian.leon@WK-LEOA-2>
2024-06-21 08:33:48 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DxListModule, DxTemplateModule } from 'devextreme-angular';
+import { RouterModule } from '@angular/router';
+import DataSource from 'devextreme/data/data_source';
+import ArrayStore from 'devextreme/data/array_store';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,    
+    DxListModule,
+    DxTemplateModule,
+    RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'devextreme-routing';
+
+  secondaryRoute(): DataSource {
+
+    return new DataSource({
+      store: new ArrayStore({
+        data: [{
+          key: 'Group components',
+          items: [{text: 'Go first', path: 'first'}, {text: 'Go second', path: 'second'}],
+        }],
+      }),
+      map: (dataItem) => {
+        return {
+          key: dataItem.key,
+          items: dataItem.items,
+        };
+      },
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'first',
+    children: [
+      {
+        path: 'first',
+        children: [{
+            path: 'first',
+            component: FirstComponent
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: 'second',
+    children: [
+      {
+        path: 'second',
+        children: [{
+            path: 'second',
+            component: SecondComponent
+          }
+        ]
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
